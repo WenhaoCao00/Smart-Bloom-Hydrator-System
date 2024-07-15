dcf86ed32145b41c68dbd7f518477c3ffde3d610
Wenhao Cao <hankcaowenhao@gmail.com>
2024-07-15 17:06:26 +0200

    done

--- front-end/pub/publish.js
+++ front-end/pub/publish.js
@@ -57,7 +57,7 @@ function connectToBroker() {
   const clientId = "client" + Math.random().toString(36).substring(7);
 
   // Change this to point to your MQTT broker
-  const host = "ws://192.168.3.51:8080";
+  const host = "ws://192.168.3.4:8080";
 
   const options = {
     keepalive: 60,
